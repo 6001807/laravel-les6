4333a418ed07d1d2ab406f04eb6b5fc49ae2b11f
JEsper <6001807@mborijnland.nl>
2024-05-07 15:43:01 +0200

    change

---
+++ opdr6/resources/views/products.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    @foreach ($products as $product)
+        <h1>{{ $product->code }}</h1>
+        <p>{{ $product->name }}</p>
+        <p>{{ $product->quantity }}</p>
+        <p>{{ $product->price }}</p>
+        <p>{{ $product->description }}</p>
+        <a href="{{ $product->code }}">Link to product</a>
+    @endforeach  
+</body>
+</html>
